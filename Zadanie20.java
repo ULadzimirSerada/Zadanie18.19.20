c01b2cdec8619282f16fc09bb3d6e9b3627a54a1
UladzimirSerada <medavikiby@gmail.com>
2021-02-22 20:40:48 +0300

    (z20)

---
+++ Zadanie20.java
@@ -0,0 +1,17 @@
+Задание 20
+Имеется строка с текстом. Вывести текст, составленный из последних букв
+всех слов.
+public class Zadanie20 {
+
+    public static void main(String[] args) {
+
+        String str = "Вот вляпался попробуй разберись";
+
+        System.out.print(str.charAt(2));
+        System.out.print(str.charAt(11));
+        System.out.print(str.charAt(20));
+        System.out.print(str.charAt(30));
+    }
+}
+/*тяйь
+Process finished with exit code 0*/
